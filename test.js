df9ff6412cbfaeb7d93b5f1f43e86836f870abc3
MelrickDavison <115827008+MelrickDavison@users.noreply.github.com>
2023-07-01 21:12:40 -0300

    Update test.js

--- test.js
+++ test.js
@@ -5,5 +5,12 @@ const {
   } = require('../src/crud');
 
 describe("Instudo", function(){
-    describe("createItem(")
+    describe("createItem()", function(){
+    it("Adicionar um aluno na turma", function(){
+        let a = new Instudo;
+        let aluno = new Aluno("Fabricio", 112, "Fabricio1313"
+        a.adicionarAluno(aluno);
+        assert.strictEqual(a.turma.length, 3);
+    })
+    } 
 })
